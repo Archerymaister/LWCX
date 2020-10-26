02a52fbf360a34bc310715f6e0e830e80b92ffae
Lukas <30570031+Archerymaister@users.noreply.github.com>
2020-10-26 09:43:51 +0100

    Update DoorMatcher.java
    Add crimson door and warped door

--- src/main/java/com/griefcraft/util/matchers/DoorMatcher.java
+++ src/main/java/com/griefcraft/util/matchers/DoorMatcher.java
@@ -67,7 +67,7 @@ public class DoorMatcher implements ProtectionFinder.Matcher {
     static {
         PROTECTABLES_DOORS = EnumSet.of(Material.OAK_DOOR, Material.SPRUCE_DOOR,
                 Material.BIRCH_DOOR, Material.ACACIA_DOOR, Material.JUNGLE_DOOR, Material.DARK_OAK_DOOR,
-                Material.IRON_DOOR);
+                Material.IRON_DOOR, Material.WARPED_DOOR, Material.CRIMSON_DOOR);
         WOODEN_DOORS = EnumSet.of(Material.OAK_DOOR, Material.SPRUCE_DOOR,
                 Material.BIRCH_DOOR, Material.ACACIA_DOOR, Material.JUNGLE_DOOR, Material.DARK_OAK_DOOR);
         FENCE_GATES = EnumSet.of(Material.OAK_FENCE_GATE, Material.SPRUCE_FENCE_GATE,
